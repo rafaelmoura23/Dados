9896461a62bc40c0379daa603c456442f56babe8
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-11-03 10:04:21 -0300

    certifieds

---
+++ Data_Analysis-Google/Part8-Projeto final/README.md
@@ -0,0 +1,2 @@
+# Case-Study_Google
+Estudo de caso do curso de análise de dados da google
---
+++ Data_Analysis-Google/Part8-Projeto final/exploração,análise e visualização.R
@@ -0,0 +1,10 @@
+# dataset = dados de futebol
+# usando o view para mostrar os dados em formato de tabela-original
+View(dataset)
+
+# instalando o pacote do tydiverse para manipulação, exploração e visualização dos dados
+install.packages("tydiverse")
+
+# str para um overview dos dados/colunas
+str(dataset) 
+
